8e208aa021f8238169f578c60dcfa9e651d3b033
Narges Moeini <narges.moeini04@gmail.com>
2016-01-30 03:09:01 -0800

    tst

---
+++ Appointer/Controllers/SignupController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Appointer.Models;
+
+namespace Appointer.Controllers
+{
+    public class SignupController : Controller
+    {
+        private AppointerEntities db = new AppointerEntities();
+
+        //
+        // GET: /Signup/
+
+        public ActionResult Index()
+        {
+            var users = db.Users.Include(u => u.Jobs1);
+            return View(users.ToList());
+        }
+
+        //
+        // GET: /Signup/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(users);
+        }
+
+        //
+        // GET: /Signup/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.JobId = new SelectList(db.Jobs, "Id", "Title");
+            return View();
+        }
+
+        //
+        // POST: /Signup/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Users users)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(users);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.JobId = new SelectList(db.Jobs, "Id", "Title", users.JobId);
+            return View(users);
+        }
+
+        //
+        // GET: /Signup/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.JobId = new SelectList(db.Jobs, "Id", "Title", users.JobId);
+            return View(users);
+        }
+
+        //
+        // POST: /Signup/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Users users)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(users).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.JobId = new SelectList(db.Jobs, "Id", "Title", users.JobId);
+            return View(users);
+        }
+
+        //
+        // GET: /Signup/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(users);
+        }
+
+        //
+        // POST: /Signup/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Users users = db.Users.Find(id);
+            db.Users.Remove(users);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
